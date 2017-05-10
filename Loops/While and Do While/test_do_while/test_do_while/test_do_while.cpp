2463a3daba8ced735b9600aff089441844eeea88
glashunti <gleibsonglas2@yahoo.com.br>
2017-05-10 19:26:19 -0300

    Commit folders

---
+++ Loops/While and Do While/test_do_while/test_do_while/test_do_while.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+	int k = 8;
+	do
+		std::cout << "K = " << k << std::endl;
+
+	while (k++ < 5);
+
+	return 0;
+
+}
---
+++ Loops/While and Do While/test_while/test_while/test_while.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+
+int main()
+{
+	int j = 5;
+	while (++j < 9) 
+		            
+	
+		std::cout << j++ << std::endl; 
+	
+	return 0;
+
+		/*
+		In the first way display the pair numbers between 5 and 9
+			int j = 5;
+	        while (++j < 9) 	
+		    std::cout << ++j << std::endl;
+
+			second way display unpaired nmmbers between 5 and 9 
+		*/
+
+}
