fa0608b8baa597f12ee9215fd2a87bc039a2a9e4
glashunti <gleibsonglas2@yahoo.com.br>
2017-05-10 16:38:40 -0300

    Better result
    Which wrong yet but doesn't return erro message from the Operational
    System

--- Loops/Program_Exercises/Program_Exercise_4/Program_Exercise_4/Program_Exercise_4.cpp
+++ Loops/Program_Exercises/Program_Exercise_4/Program_Exercise_4/Program_Exercise_4.cpp
@@ -7,17 +7,17 @@ int main()
     int years = 0;
 
 	original_balance = 100;
-	current_balance = 100;
 	
 	
-	for (interest_cleo = 100 ; interest_cleo <= interest_daphne; interest_cleo++)
+	
+	for (current_balance = 100 ; current_balance <= original_balance; current_balance++)
 	{
 		
 	    interest_cleo = 0.05*current_balance;
 		current_balance += interest_cleo;
 	    interest_daphne = 0.10*100;
 		original_balance += interest_daphne;
-		//years++;
+		years++;
 	}
 
 	std::cout << interest_cleo << " interest of Cleo " << interest_daphne << " interest of Daphne " << years << "years" << std::endl;
